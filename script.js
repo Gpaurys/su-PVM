b4b23f7ce917bf96c77b6f796b76d5e73a13543f
Giedrius <paurys79@gmail.com>
2021-06-02 19:03:46 +0300

    Įkėliau su PVM

---
+++ script.js
@@ -0,0 +1,20 @@
+prompt("Kokia prekės kaina?");
+
+
+var price = 23.07;
+var PVM = 4.8447;
+var priceNoPVM = 18.2253;
+var sum = priceNoPVM + PVM;
+console.log(sum.toFixed(2) + "€ ");
+
+// var price = 23.07;
+// var PVM = 4.8447;
+// var priceNoPVM = 18.2253;
+// var minus = price - priceNoPVM;
+// console.log(minus.toFixed(2) + "€ ");
+
+// var price = 23.07;
+// var PVM = 4.8447;
+// var priceNoPVM = 18.2253;
+// var minus = price - PVM;
+// console.log(minus.toFixed(2) + "€ ");
